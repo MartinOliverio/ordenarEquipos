255ef2d8a3e1db5c588df7986dc0eabdeb83b923
MartinOliverio <54281030+MartinOliverio@users.noreply.github.com>
2025-07-18 15:34:57 -0300

    Add files via upload

---
+++ ordenar-equipos.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+import random
+
+app = Flask(__name__)
+
+# Tus equipos
+equipos = ["Catalogo", "Compras", "Contabilidad", "Growth", "Informes", "Integraciones", "POS", "Stock", "Ventas"]
+
+@app.route('/orden-presentacion', methods=['POST'])
+def orden_presentacion():
+    shuffled = equipos[:]
+    random.shuffle(shuffled)
+    mensaje = "*🎤 Orden aleatorio de presentación:* \n"
+    mensaje += "\n".join(f"{i+1}. {e}" for i, e in enumerate(shuffled))
+    return jsonify({
+        "response_type": "in_channel",  # Para que lo vea todo el canal
+        "text": mensaje
+    })
+
+if __name__ == '__main__':
+    app.run(port=3000)
